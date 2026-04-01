6657e32fd05d6b551cd1951f8f04f83ac60cfdde
Ho-jin98 <hohoj98@naver.com>
2026-04-01 16:48:55 +0900

    feat: step 4 테스트 코드 수정

--- src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -9,11 +9,13 @@ import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.swing.*;
 import java.time.LocalDateTime;
 
 import static org.mockito.Mockito.when;
@@ -69,9 +71,19 @@ class TodoControllerTest {
 
         // then
         mockMvc.perform(get("/todos/{todoId}", todoId))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status").value(HttpStatus.OK.name()))
-                .andExpect(jsonPath("$.code").value(HttpStatus.OK.value()))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.name()))
+                .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.value()))
                 .andExpect(jsonPath("$.message").value("Todo not found"));
     }
 }
+/*
+ given -> 테스트 사전 조건 설정, "~가 주어졌을 때" 즉 테스트에 필요한 데이터나 mock 설정을 준비하는 단계
+ when -> 실제 테스트할 동작을 실행하는 단계, "~을 실행했을 때" 즉, 메서드를 호출하는 단계
+ then -> 결과 겸증 단계, "~이어야 한다." 주장함, 즉 실행 결과가 예상과 맞는지 확인하는 단계
+
+ build.gradle에 security 의존성 추가가 되어 있으면 400이 안떨어지고 401로 떨어지려함
+
+ MockMvc에서 Security필터를 비활성화하는 어노테이션
+ Spring Security도 필터 기반으로 동작하기 때문에 이 설정으로 Security 인증 과정을 건너뛸 수 있음.
+ @AutoConfigureMockMvc(addFilters = false) */
